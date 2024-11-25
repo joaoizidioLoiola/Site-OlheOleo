406b7c788242dd2173a840c78dd0231001f9f13c
Caique Leme <caiquealeme@gmail.com>
2024-11-25 17:49:05 -0300

    fix: layout do footer

--- src/app/TelaLogin/TelaHome/TelaPerfil/components/FooterNavigation.tsx
+++ src/app/TelaLogin/TelaHome/TelaPerfil/components/FooterNavigation.tsx
@@ -2,18 +2,19 @@ import Image from 'next/image';
 
 const FooterNavigation = () => {
     return (
-        <footer className="bg-txt w-full h-1/5 flex flex-col justify-between fixed bottom-0">
-            <div className="flex items-center justify-center text p-2">
+        <footer className="bg-txt w-full h-1/5 flex flex-col justify-around fixed items-around bottom-0 p-2 ">
+            <div className="flex items-center justify-between text-[#FFF5DD]">
                 <div className="flex items-center">
                     <Image src="/logo.png" alt="Ícone" width={150} height={60} />
                 </div>
-                <div className="flex flex-col items-center text-sm md:text-base">
-                    <span>Av. Bahia, 1739 - Indaiá, Caraguatatuba - SP, 11665-071</span>
+                <div className="flex flex-col items-end text-sm md:text-base">
+                    <span>Av. Bahia, 1739 - Indaiá</span>
+                    <span>Caraguatatuba - SP, 11665-071</span>
                     <span>olheeoleo@gmail.com.br</span>
-                    <span>(00) 0 0000-0000 | (00) 0 0000-0000</span>
+                    <span>(00) 0 0000-0000</span>
                 </div>
             </div>
-            <div className="text-center pb-2">
+            <div className="text-end text-[#FFF5DD]">
                 <span className="text-xs md:text-sm">Copyright © 2024</span>
             </div>
         </footer>
